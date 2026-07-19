5f538a1a82e2296c26f8b7cb08d39c11e0c54642
Karrar Al-Jawasim <kaljawasim@gmail.com>
2026-07-19 12:19:39 +0300

    test: add unit test to BookingController, and BookingService

--- src/features/booking/booking.controller.spec.ts
+++ src/features/booking/booking.controller.spec.ts
@@ -1,18 +1,133 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookingController } from './booking.controller';
+import { BookingService } from './booking.service';
+import { BookingRows } from './schema/booking.schema';
+import { CreateBookingDto } from './dto/create-booking.dto';
+import { Role } from '../user/enums/user-role.enum';
+import { PublicUser } from '../user/types/public-user.types';
+import { BookingStatus } from './enums/booking-status.enum';
 
 describe('BookingController', () => {
-  let controller: BookingController;
+  let bookingController: BookingController;
+  let bookingService: BookingService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BookingController],
+      providers: [
+        {
+          provide: BookingService,
+          useValue: {
+            create: jest.fn(),
+            getById: jest.fn(),
+            cancelBooking: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<BookingController>(BookingController);
+    bookingController = module.get<BookingController>(BookingController);
+    bookingService = module.get<BookingService>(BookingService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('create', () => {
+    it('should create booking and return it ', async () => {
+      jest.spyOn(bookingService, 'create').mockResolvedValue({
+        id: 'booking 1',
+        userId: 'user 1',
+        quantity: 3,
+        inventoryItemId: 'inventory 1',
+      } as BookingRows);
+
+      const result = await bookingController.create(
+        {
+          quantity: 3,
+          inventoryItemId: 'inventory 1',
+        } as CreateBookingDto,
+        'user 1',
+      );
+      expect(bookingService.create).toHaveBeenCalledWith(
+        {
+          quantity: 3,
+          inventoryItemId: 'inventory 1',
+        },
+        'user 1',
+      );
+
+      expect(result).toEqual({
+        id: 'booking 1',
+        userId: 'user 1',
+        quantity: 3,
+        inventoryItemId: 'inventory 1',
+      });
+    });
+  });
+
+  describe('getById', () => {
+    it('should return booking if exist', async () => {
+      jest.spyOn(bookingService, 'getById').mockResolvedValue({
+        id: 'booking 1',
+        userId: 'user 1',
+        quantity: 3,
+        inventoryItemId: 'inventory 1',
+      } as BookingRows);
+
+      const result = await bookingController.getById('booking 1', {
+        id: 'user 1',
+        role: Role.User,
+      } as PublicUser);
+      expect(bookingService.getById).toHaveBeenCalledWith(
+        'booking 1',
+        'user 1',
+        Role.User,
+      );
+
+      expect(result).toEqual({
+        id: 'booking 1',
+        userId: 'user 1',
+        quantity: 3,
+        inventoryItemId: 'inventory 1',
+      });
+    });
+  });
+
+  describe('cancelBooking', () => {
+    it('should update booking status to canceled', async () => {
+      jest.spyOn(bookingService, 'getById').mockResolvedValue({
+        id: 'booking 1',
+        userId: 'user 1',
+        quantity: 3,
+        inventoryItemId: 'inventory 1',
+        status: BookingStatus.Pending,
+      } as BookingRows);
+
+      jest.spyOn(bookingService, 'cancelBooking').mockResolvedValue({
+        id: 'booking 1',
+        userId: 'user 1',
+        quantity: 3,
+        inventoryItemId: 'inventory 1',
+        status: BookingStatus.Canceled,
+      } as BookingRows);
+
+      const result = await bookingController.cancelBooking('booking 1', {
+        id: 'user 1',
+        role: Role.User,
+      } as PublicUser);
+      expect(bookingService.cancelBooking).toHaveBeenCalledWith(
+        'booking 1',
+        'user 1',
+        Role.User,
+      );
+
+      expect(result).toEqual({
+        id: 'booking 1',
+        userId: 'user 1',
+        quantity: 3,
+        inventoryItemId: 'inventory 1',
+        status: BookingStatus.Canceled,
+      });
+    });
   });
 });
--- src/features/booking/booking.service.spec.ts
+++ src/features/booking/booking.service.spec.ts
@@ -1,18 +1,183 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookingService } from './booking.service';
+import { BookingRepository } from './booking.repository';
+import { BookingRows } from './schema/booking.schema';
+import { Role } from '../user/enums/user-role.enum';
+import { InventoryRedisService } from '../inventory/inventory-redis.service';
+import { ForbiddenException, NotFoundException } from '@nestjs/common';
+import { Queue } from 'bullmq';
+import { getQueueToken } from '@nestjs/bullmq';
+import { BookingStatus } from './enums/booking-status.enum';
 
 describe('BookingService', () => {
-  let service: BookingService;
+  let bookingService: BookingService;
+  let bookingRepository: BookingRepository;
+  let inventoryRedisService: InventoryRedisService;
+  let bookingQueue: Queue;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BookingService],
+      providers: [
+        BookingService,
+        {
+          provide: BookingRepository,
+          useValue: {
+            findById: jest.fn(),
+            create: jest.fn(),
+            cancelBooking: jest.fn(),
+            findBookingsByUserId: jest.fn(),
+          },
+        },
+        {
+          provide: InventoryRedisService,
+          useValue: {
+            release: jest.fn(),
+            reserve: jest.fn(),
+            syncInventoryToRedis: jest.fn(),
+          },
+        },
+        {
+          provide: getQueueToken('booking_expiry'),
+          useValue: {
+            add: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<BookingService>(BookingService);
+    bookingService = module.get<BookingService>(BookingService);
+    bookingRepository = module.get<BookingRepository>(BookingRepository);
+    inventoryRedisService = module.get<InventoryRedisService>(
+      InventoryRedisService,
+    );
+    bookingQueue = module.get<Queue>(getQueueToken('booking_expiry'));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getById', () => {
+    it('should return booking if exist', async () => {
+      jest.spyOn(bookingRepository, 'findById').mockResolvedValue({
+        id: 'booking 1',
+        userId: 'user 1',
+      } as BookingRows);
+
+      const result = await bookingService.getById(
+        'booking 1',
+        'user 1',
+        Role.User,
+      );
+      expect(bookingRepository.findById).toHaveBeenCalledWith('booking 1');
+      expect(result).toEqual({
+        id: 'booking 1',
+        userId: 'user 1',
+      });
+    });
+
+    it('should throw NotFoundException if booking not exist', async () => {
+      jest.spyOn(bookingRepository, 'findById').mockResolvedValue(undefined);
+      await expect(
+        bookingService.getById('not_exist_id', 'user 1', Role.User),
+      ).rejects.toThrow(NotFoundException);
+    });
+
+    it('should throw ForbiddenException if userId not equal userId in booking and role not admin ', async () => {
+      jest.spyOn(bookingRepository, 'findById').mockResolvedValue({
+        id: 'booking 1',
+        userId: 'user 1',
+      } as BookingRows);
+      await expect(
+        bookingService.getById('booking 1', 'user 2', Role.User),
+      ).rejects.toThrow(ForbiddenException);
+    });
+  });
+
+  describe('cancelBooking', () => {
+    it('should update booking status to canceled', async () => {
+      jest.spyOn(bookingRepository, 'findById').mockResolvedValue({
+        id: 'booking 1',
+        userId: 'user 1',
+        status: BookingStatus.Pending,
+      } as BookingRows);
+
+      jest.spyOn(bookingRepository, 'cancelBooking').mockResolvedValue({
+        id: 'booking 1',
+        userId: 'user 1',
+        status: BookingStatus.Canceled,
+      } as BookingRows);
+
+      const result = await bookingService.cancelBooking(
+        'booking 1',
+        'user 1',
+        Role.User,
+      );
+      expect(bookingRepository.cancelBooking).toHaveBeenCalledWith('booking 1');
+      expect(result).toEqual({
+        id: 'booking 1',
+        userId: 'user 1',
+        status: BookingStatus.Canceled,
+      });
+    });
+
+    it('should throw NotFoundException if booking not exist', async () => {
+      jest.spyOn(bookingRepository, 'findById').mockResolvedValue(undefined);
+      await expect(
+        bookingService.cancelBooking('not_exist_id', 'user 1', Role.User),
+      ).rejects.toThrow(NotFoundException);
+    });
+
+    it('should throw ForbiddenException if userId not equal userId in booking and role not admin ', async () => {
+      jest.spyOn(bookingRepository, 'findById').mockResolvedValue({
+        id: 'booking 1',
+        userId: 'user 1',
+      } as BookingRows);
+      await expect(
+        bookingService.cancelBooking('booking 1', 'user 2', Role.User),
+      ).rejects.toThrow(ForbiddenException);
+    });
+  });
+
+  describe('getBookingsByUserId', () => {
+    it('should return all bookings to the user', async () => {
+      jest.spyOn(bookingRepository, 'findBookingsByUserId').mockResolvedValue({
+        items: [
+          {
+            id: 'booking 1',
+            userId: 'user 1',
+          } as BookingRows,
+          {
+            id: 'booking 2',
+            userId: 'user 1',
+          } as BookingRows,
+        ],
+        totalCountResult: 2,
+      });
+
+      const skip = 0;
+      const limit = 10;
+      const result = await bookingService.getBookingsByUserId(
+        'user 1',
+        skip,
+        limit,
+      );
+      expect(bookingRepository.findBookingsByUserId).toHaveBeenCalledWith(
+        'user 1',
+        skip,
+        limit,
+      );
+      expect(result).toEqual({
+        items: [
+          {
+            id: 'booking 1',
+            userId: 'user 1',
+          } as BookingRows,
+          {
+            id: 'booking 2',
+            userId: 'user 1',
+          } as BookingRows,
+        ],
+        totalCountResult: 2,
+      });
+    });
   });
 });
